89ad8be13eabdc29c7a7cb860739ab5633d5b68e
Josh Montoya <josh@montoya.io>
2022-06-10 10:41:24 -0700

    implement tests for New()

--- configuration/configuration_test.go
+++ configuration/configuration_test.go
@@ -1,5 +1,11 @@
 package configuration
 
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
 const (
 	exampleJSON = `
 {
@@ -10,6 +16,13 @@ const (
 }
 `
 
+	exampleJSONInvalid = `
+{
+	"aws_role_arn" : "2",
+	"okta_client_id" : "3",
+	"okta_url" : "4"				
+}
+`
 	exampleJSONArray = `["hello world", "foo", "bar", "baz"]`
 
 	exampleTOML = `
@@ -19,3 +32,75 @@ okta_client_id = "3"
 okta_url = "4"
 `
 )
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name string
+		prep func() (toRemove *os.File, err error)
+
+		wantCfg *Configuration
+		wantErr bool
+	}{
+		{
+			name: "success",
+			prep: func() (toRemove *os.File, err error) {
+				toRemove, err = createTestTempFile(exampleJSON)
+				os.Stdin = toRemove
+				return
+			},
+			wantCfg: &Configuration{
+				AWSProviderARN: "1",
+				AWSRoleARN:     "2",
+				OktaClientID:   "3",
+				OktaURL:        "4",
+			},
+		},
+		{
+			name: "failure (invalid configuration)",
+			prep: func() (toRemove *os.File, err error) {
+				toRemove, err = createTestTempFile(exampleJSONInvalid)
+				os.Stdin = toRemove
+				return
+			},
+			wantErr: true,
+		},
+		{
+			name: "failure (closed file)",
+			prep: func() (toRemove *os.File, err error) {
+				toRemove, err = createTestClosedTempfile()
+				os.Stdin = toRemove
+				return
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var (
+				toRemove *os.File
+				err      error
+			)
+
+			if toRemove, err = tt.prep(); err != nil {
+				t.Errorf("New() error preparing test: %v", err)
+				return
+			}
+
+			if toRemove != nil {
+				defer os.Remove(toRemove.Name())
+				defer toRemove.Close()
+			}
+
+			gotCfg, err := New()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(gotCfg, tt.wantCfg) {
+				t.Errorf("New() = %v, want %v", gotCfg, tt.wantCfg)
+			}
+		})
+	}
+}
