4157b347fc34ef93abc86dd804018d66d7d796b7
Josh Montoya <josh@montoya.io>
2022-06-10 09:15:24 -0700

    implement decode unit tests

--- configuration/utils_test.go
+++ configuration/utils_test.go
@@ -1,11 +1,33 @@
 package configuration
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"io/ioutil"
 	"os"
+	"reflect"
 	"testing"
+)
+
+const (
+	exampleJSON = `
+{
+	"aws_provider_arn" : "1",
+	"aws_role_arn" : "2",
+	"okta_client_id" : "3",
+	"okta_url" : "4"				
+}
+`
+
+	exampleJSONArray = `["hello world", "foo", "bar", "baz"]`
+
+	exampleTOML = `
+aws_provider_arn = "1"
+aws_role_arn = "2"
+okta_client_id = "3"
+okta_url = "4"
+`
 )
 
 func Test_getSource(t *testing.T) {
@@ -225,6 +247,151 @@ func Test_getStdinReader(t *testing.T) {
 			gotString := string(gotBS)
 			if gotString != tt.wantContents {
 				t.Fatalf("getStdinReader() received = <%s>, wantContents = <%s>", gotString, tt.wantContents)
+			}
+		})
+	}
+}
+
+func Test_parseReader(t *testing.T) {
+	type args struct {
+		r io.ReadSeeker
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantCfg *Configuration
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotCfg, err := parseReader(tt.args.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseReader() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotCfg, tt.wantCfg) {
+				t.Errorf("parseReader() = %v, want %v", gotCfg, tt.wantCfg)
+			}
+		})
+	}
+}
+
+func Test_decodeAsTOML(t *testing.T) {
+	type args struct {
+		r io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantCfg *Configuration
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				r: bytes.NewBufferString(exampleTOML),
+			},
+			wantCfg: &Configuration{
+				AWSProviderARN: "1",
+				AWSRoleARN:     "2",
+				OktaClientID:   "3",
+				OktaURL:        "4",
+			},
+		},
+		{
+			name: "failure (json array)",
+			args: args{
+				r: bytes.NewBufferString(exampleJSONArray),
+			},
+			wantErr: true,
+		},
+		{
+			name: "failure (json object)",
+			args: args{
+				r: bytes.NewBufferString(exampleJSON),
+			},
+			wantErr: true,
+		},
+		{
+			name: "failure (plaintext)",
+			args: args{
+				r: bytes.NewBufferString(`hello world`),
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotCfg, err := decodeAsTOML(tt.args.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("decodeAsTOML() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotCfg, tt.wantCfg) {
+				t.Errorf("decodeAsTOML() = %v, want %v", gotCfg, tt.wantCfg)
+			}
+		})
+	}
+}
+
+func Test_decodeAsJSON(t *testing.T) {
+	type args struct {
+		r io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantCfg *Configuration
+		wantErr bool
+	}{
+		{
+			name: "success",
+			args: args{
+				r: bytes.NewBufferString(exampleJSON),
+			},
+			wantCfg: &Configuration{
+				AWSProviderARN: "1",
+				AWSRoleARN:     "2",
+				OktaClientID:   "3",
+				OktaURL:        "4",
+			},
+		},
+		{
+			name: "failure (array)",
+			args: args{
+				r: bytes.NewBufferString(exampleJSONArray),
+			},
+			wantErr: true,
+		},
+		{
+			name: "failure (toml)",
+			args: args{
+				r: bytes.NewBufferString(exampleTOML),
+			},
+			wantErr: true,
+		},
+		{
+			name: "failure (plaintext)",
+			args: args{
+				r: bytes.NewBufferString(`hello world`),
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotCfg, err := decodeAsJSON(tt.args.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("decodeAsJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotCfg, tt.wantCfg) {
+				t.Errorf("decodeAsJSON() = %v, want %v", gotCfg, tt.wantCfg)
 			}
 		})
 	}
