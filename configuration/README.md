b927543d5dbba2c23a999ab9bdc3c603d428f32a
Josh Montoya <josh@montoya.io>
2022-06-10 15:28:00 -0700

    add examples for OverrideWith

--- configuration/README.md
+++ configuration/README.md
@@ -30,6 +30,28 @@ func ExampleNew_with_override() {
 }
 ```
 
+### Configuration.OverrideWith
+```go
+func ExampleConfiguration_OverrideWith() {
+	cfg := Configuration{
+		AWSProviderARN: "1",
+		AWSRoleARN:     "2",
+		OktaClientID:   "3",
+		OktaAppID:      "4",
+		OktaURL:        "5",
+	}
+
+	overrides := Configuration{
+		AWSProviderARN: "1new",
+		AWSRoleARN:     "2new",
+	}
+
+	cfg.OverrideWith(&overrides)
+
+	fmt.Printf("Updated values: %+v\n", cfg)
+}
+```
+
 ### Configuration.Validate
 ```go
 func ExampleConfiguration_Validate() {
--- configuration/configuration_test.go
+++ configuration/configuration_test.go
@@ -407,6 +407,25 @@ func ExampleNew_with_override() {
 	fmt.Println("We have our configuration!", cfg)
 }
 
+func ExampleConfiguration_OverrideWith() {
+	cfg := Configuration{
+		AWSProviderARN: "1",
+		AWSRoleARN:     "2",
+		OktaClientID:   "3",
+		OktaAppID:      "4",
+		OktaURL:        "5",
+	}
+
+	overrides := Configuration{
+		AWSProviderARN: "1new",
+		AWSRoleARN:     "2new",
+	}
+
+	cfg.OverrideWith(&overrides)
+
+	fmt.Printf("Updated values: %+v\n", cfg)
+}
+
 func ExampleConfiguration_Validate() {
 	cfg := Configuration{
 		AWSProviderARN: "1",
