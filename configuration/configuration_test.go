71d92cf15ac215d96cdabe70a12d90c284e2d281
Josh Montoya <josh@montoya.io>
2022-06-10 10:29:05 -0700

    move testing const to configuration_test.go

---
+++ configuration/configuration_test.go
@@ -0,0 +1,21 @@
+package configuration
+
+const (
+	exampleJSON = `
+{
+	"aws_provider_arn" : "1",
+	"aws_role_arn" : "2",
+	"okta_client_id" : "3",
+	"okta_url" : "4"				
+}
+`
+
+	exampleJSONArray = `["hello world", "foo", "bar", "baz"]`
+
+	exampleTOML = `
+aws_provider_arn = "1"
+aws_role_arn = "2"
+okta_client_id = "3"
+okta_url = "4"
+`
+)
--- configuration/utils_test.go
+++ configuration/utils_test.go
@@ -11,26 +11,6 @@ import (
 	"testing"
 )
 
-const (
-	exampleJSON = `
-{
-	"aws_provider_arn" : "1",
-	"aws_role_arn" : "2",
-	"okta_client_id" : "3",
-	"okta_url" : "4"				
-}
-`
-
-	exampleJSONArray = `["hello world", "foo", "bar", "baz"]`
-
-	exampleTOML = `
-aws_provider_arn = "1"
-aws_role_arn = "2"
-okta_client_id = "3"
-okta_url = "4"
-`
-)
-
 func Test_getConfiguration(t *testing.T) {
 	tests := []struct {
 		name    string
