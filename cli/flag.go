c08210eb74fbf73fb1fbe8850c5f56b663768e61
Angeles <maria.ramirez@wizeline.com>
2022-06-13 09:54:42 -0500

    feat: [ARTCSP-205] implement cli

---
+++ cli/flag.go
@@ -0,0 +1,30 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+)
+
+const (
+	FlagProfile = "profile"
+)
+
+func (c CLI) ParseFlags() FlagMap {
+	// Define all flags here, then add them to the map
+	pFlag := flag.String(FlagProfile, "", "profile to use in command")
+	flag.Parse()
+
+	return map[string]Flag{
+		FlagProfile: {
+			Name:  FlagProfile,
+			Value: *pFlag,
+		},
+	}
+}
+
+func findFlag(name string, flags FlagMap) (Flag, error) {
+	if f, ok := flags[name]; ok {
+		return f, nil
+	}
+	return Flag{}, fmt.Errorf("flag %w: %s", ErrNotFound, name)
+}
